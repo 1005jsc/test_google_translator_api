e752e4a906aff3c95cace59fce7c7becb1e127bc
Jae Sin Cho <1005jsc@naver.com>
2025-04-03 09:38:47 +0900

    4월 3일

---
+++ src/Pages/TranslatorPage.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import { translateTextFromGoogle } from '../utils/GoogleTranstlatorApi';
+
+type TranslatorPageProps = {};
+
+const TranslatorPage = ({}: TranslatorPageProps) => {
+  const [text, setText] = useState<string>('');
+  const [translatedText, setTranslatedText] = useState<string | undefined>('');
+
+  const handleTranslate = async () => {
+    const yo = await translateTextFromGoogle(text);
+    setTranslatedText(yo);
+  };
+
+  return (
+    <div>
+      <h2>Google Translate API Test</h2>
+      <textarea
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="번역할 내용을 입력하세요"
+        rows={4}
+        cols={50}
+      />
+      <br />
+      <button onClick={handleTranslate}>번역하기</button>
+      <h3>번역 결과:</h3>
+      <p>{translatedText}</p>
+    </div>
+  );
+};
+
+export default TranslatorPage;
